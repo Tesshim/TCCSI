781f025bda0eb3316206975f32a15a7d9d1391b4
Filipe Gomes <filippedocs@gmail.com>
2018-06-25 18:02:21 -0300

    Add new files
    New files.

---
+++ SMSPSDMO.h
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   SMSPSDMO.h
+ * Author: marcelo
+ *
+ * Created on 26 de Abril de 2017, 10:09
+ */
+
+#ifndef SMSPSDMO_H
+#define SMSPSDMO_H
+
+#include "/opt/gurobi702/linux64/include/gurobi_c++.h"
+#include "SchedulingInstance.h"
+#include "Utils.h"
+#include "SchedulingInstance.h"
+#include <math.h>
+
+using namespace std;
+
+class SMSPSDMO {
+public:
+    // Create environment
+    GRBEnv env = GRBEnv();
+
+    // Create a new model
+    GRBModel model = GRBModel(env);
+    
+    double up = 100000;
+    
+    void ExactTotalTardinessAndMakeSpan(SchedulingInstance Instance);
+private:
+
+};
+
+#endif /* SMSPSDMO_H */
+
